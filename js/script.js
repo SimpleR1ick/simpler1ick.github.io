a91ad8de12853814a3564ddf8a7294784688f352
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2023-03-25 17:37:36 -0300

    Routes test 2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const url = "https://simpler1ick.github.io/"
+
+function pageCurriculum() {
+    location.href = url + "?page=curriculum";
+}
+
+function pageContacts() {
+    location.href = url + "?page=contacts";
+}
+
+function pageProjects() {
+    location.href = url + "?page=projects";
+}

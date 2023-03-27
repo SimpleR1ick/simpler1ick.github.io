ee02bd541dd0d909adecd635976884a91060405c
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2023-03-26 23:15:09 -0300

    Link fix

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,24 @@
+// URL DO SITE
 const url = "https://simpler1ick.github.io/";
 
+// LINKS DA NAVBAR
+$("#lk-nav").click(function () {
+    location.href = url;
+});
+
+$("#lk-curriculo").click(function () {
+    location.href = url + "?dc=curriculum";
+});
+
+$("#lk-projetos").click(function () {
+    location.href = url + "?dc=projects";
+});
+
+$("#lk-contatos").click(function () {
+    location.href = url + "?dc=contacts";
+});
+
+// BOTOES DA HOME
 $("#btn-curriculo").click(function () {
     location.href = url + "?dc=curriculum";
 });
--- js/contenct.js
+++ js/contenct.js
@@ -1,4 +1,4 @@
-// Parse the URL parameter
+// Analisa o parâmetro de URL
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
 
@@ -6,28 +6,31 @@ function getParameterByName(name, url) {
 
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
+
     if (!results) return null;
+    
     if (!results[2]) return "";
 
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-// Give the parameter a variable name
+
+// Dê ao parâmetro um nome de variável
 var dynamicContent = getParameterByName("dc");
 
 $(document).ready(function () {
-    // Check if the URL parameter is apples
+    // Verifique se o parâmetro de URL é currículo
     if (dynamicContent == 'curriculum') {
         $('#curriculum').show();
     }
-    // Check if the URL parameter is oranges
+    // Verifique se o parâmetro de URL é projetos
     else if (dynamicContent == 'projects') {
         $('#projects').show();
     }
-    // Check if the URL parameter is bananas
+    // Verifique se o parâmetro de URL é contatos
     else if (dynamicContent == 'contacts') {
         $('#contacts').show();
     }
-    // Check if the URL parmeter is empty or not defined, display default content
+    // Verifica se o parâmetro de URL está vazio ou não definido, exibe o conteúdo padrão
     else {
         $('#default').show();
     }
